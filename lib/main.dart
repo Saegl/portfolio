f500f093a9bf7f7730d636ca736bb2e8b38b5d35
Saegl <saegl@protonmail.com>
2021-12-11 14:09:08 +0600

    some

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ void main() {
   runApp(ResumeEditor());
 }
 
+TextStyle name = TextStyle(fontSize: 26.0);
+TextStyle h1 = TextStyle(fontSize: 20.0);
+TextStyle h2 = TextStyle(fontSize: 16.0);
+TextStyle p = TextStyle(fontSize: 14.0);
+
 class ResumeEditor extends StatefulWidget {
   const ResumeEditor({Key? key}) : super(key: key);
 
@@ -12,24 +17,80 @@ class ResumeEditor extends StatefulWidget {
 }
 
 class _ResumeEditorState extends State<ResumeEditor> {
-  List<Widget> children = [
+  List<Widget> sections = [
     Padding(
       padding: EdgeInsets.symmetric(horizontal: 120.0),
       child: Card(
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network('https://picsum.photos/250?image=9'),
-            Column(
-              children: [
-                Text("Name Surname"),
-                Text("Computer Science"),
-                Text("GPA 4.0/4.0"),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.network(
+                'https://avatars.mds.yandex.net/get-zen_doc/49613/pub_5a6848d677d0e6434dc869d4_5a684dd8168a918051c0ea50/scale_1200',
+                width: 200.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 32.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Name Surname",
+                    style: name,
+                  ),
+                  Text("Computer Science", style: h2),
+                  Text("GPA 4.0/4.0", style: h2),
+                ],
+              ),
             ),
           ],
         ),
       ),
-    )
+    ),
+    Padding(
+      padding: const EdgeInsets.fromLTRB(120.0, 10.0, 120.0, 0.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "COURSES",
+                style: h1,
+              ),
+              Text("CSS 225 Web Programming Fundamentals", style: h2),
+              Text("- HTML, CSS, Javascript basic sites", style: p),
+              Text(
+                  "- CRUD Blog; FRONT: bootstrap, Ajax; BACK: Flask, Sqlite, JSON storage",
+                  style: p)
+            ],
+          ),
+        ),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 120.0, vertical: 10.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "AWARDS",
+                style: h1,
+              ),
+              Text("Hackaton 2021 Winner", style: h2),
+              Text("Website for online resume generation", style: p),
+            ],
+          ),
+        ),
+      ),
+    ),
+    Section("Example"),
   ];
   @override
   Widget build(BuildContext context) {
@@ -39,14 +100,58 @@ class _ResumeEditorState extends State<ResumeEditor> {
         appBar: AppBar(
           title: Text("Edit Resume"),
         ),
-        body: ListView(
-          children: children,
+        body: ListView.builder(
+          itemCount: sections.length,
+          itemBuilder: (context, int index) {
+            return sections[index];
+          },
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
             print("new section");
+            sections.add(Section("Example"));
+            print(sections.length);
+            setState(() {});
           },
+        ),
+      ),
+    );
+  }
+}
+
+class Section extends StatelessWidget {
+  const Section(
+    this.name, {
+    Key? key,
+  }) : super(key: key);
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 120.0, vertical: 10.0),
+      child: Card(
+        child: InkWell(
+          onTap: () {
+            print('ok');
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: h1,
+                ),
+                Text("ChocoDev Internship", style: h2),
+                Text(
+                    "boring description about the passed interternship/jobs and other stuff",
+                    style: p),
+              ],
+            ),
+          ),
         ),
       ),
     );
