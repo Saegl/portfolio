75d1f69f1da366f3200922f902db16a069750012
vdios <diasnurmangaliyev@gmail.com>
2021-12-11 14:08:35 +0600

    init 3

--- lib/repository.dart
+++ lib/repository.dart
@@ -1,17 +1,39 @@
 import 'models.dart';
 
 class Repository {
-
   late Student _student;
   final studentList = <Student>[];
 
-  Future<String?> loginStudent(Student student) async {
+  // hardcode
+  GeneralInfo hardcodedGeneralInfo = GeneralInfo(
+    name: 'Ahmet',
+    surname: 'Dastan',
+    gpa: 2.6,
+  );
+  //
+
+  String? loginStudent(Student student) {
+    if(!studentList.contains(student)) {
+      return 'such student do not exist';
+    }
+
     _student = student;
+    _student.setGeneralInfo = hardcodedGeneralInfo;
     return null;
   }
 
-  Future<String?> registerStudent(Student student) async {
+  String? registerStudent(Student student) {
+    if(studentList.contains(student)) {
+      return 'student already registered';
+    }
+
     studentList.add(student);
     return null;
   }
+
+  GeneralInfo getGeneralInfo() {
+    return _student.generalInfo!;
+  }
+
+
 }
