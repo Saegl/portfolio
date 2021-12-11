501d65f9f968b5b38080f01513ff5cbfc0e03d62
Saegl <saegl@protonmail.com>
2021-12-11 13:07:00 +0600

    first

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ResumeEditor());
+}
+
+class ResumeEditor extends StatefulWidget {
+  const ResumeEditor({Key? key}) : super(key: key);
+
+  @override
+  _ResumeEditorState createState() => _ResumeEditorState();
+}
+
+class _ResumeEditorState extends State<ResumeEditor> {
+  List<Widget> children = [
+    Padding(
+      padding: EdgeInsets.symmetric(horizontal: 120.0),
+      child: Card(
+        child: Row(
+          children: [
+            Image.network('https://picsum.photos/250?image=9'),
+            Column(
+              children: [
+                Text("Name Surname"),
+                Text("Computer Science"),
+                Text("GPA 4.0/4.0"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Edit Resume',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Edit Resume"),
+        ),
+        body: ListView(
+          children: children,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print("new section");
+          },
+        ),
+      ),
+    );
+  }
+}
